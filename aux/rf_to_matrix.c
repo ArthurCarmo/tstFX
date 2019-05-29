e18c40deb65291735b2da5b4de7be447faeab78a
Arthur <arthur.goncalves.carmo@gmail.com>
2019-05-28 21:26:17 -0300

    modified:   aux/rf_to_matrix.c

--- aux/rf_to_matrix.c
+++ aux/rf_to_matrix.c
@@ -15,9 +15,17 @@ int main() {
 			switch(fgetc(f)) {
 				case ' ' : printf("vazio");
 					break;
+				case 'G' : printf("green");
+					break;
 				case 'B' : printf("blue");
 					break;
 				case 'K' : printf("gold");
+					break;
+				case 'g' : printf("weak_green");
+					break;
+				case 'b' : printf("weak_blue");
+					break;
+				case 'k' : printf("weak_gold");
 					break;
 				case 'W' : printf("wall");
 					break;
